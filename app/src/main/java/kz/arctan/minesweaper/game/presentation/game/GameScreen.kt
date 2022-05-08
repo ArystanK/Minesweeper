477a88e9e67c71f3e2ef12f03018a3f35b76708e
a.kalmakhanov <HbstE25>
2022-05-08 16:38:29 +0600

    onLongPress Function is added to the tile

--- app/src/main/java/kz/arctan/minesweaper/game/presentation/game/GameScreen.kt
+++ app/src/main/java/kz/arctan/minesweaper/game/presentation/game/GameScreen.kt
@@ -1,27 +1,17 @@
 package kz.arctan.minesweaper.game.presentation.game
 
-import androidx.compose.foundation.ScrollState
-import androidx.compose.foundation.gestures.FlingBehavior
-import androidx.compose.foundation.gestures.detectTapGestures
-import androidx.compose.foundation.horizontalScroll
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.*
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.AlertDialog
-import androidx.compose.material.Button
-import androidx.compose.material.Text
-import androidx.compose.material.TextButton
+import androidx.compose.material.*
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -37,8 +27,10 @@ fun GameScreenPreview() {
         playgroundData = PlaygroundGenerationUseCase().invoke(10, 8, 10),
         width = 8,
         height = 10,
-        bombs = 10
-    ) {}
+        bombs = 10,
+        playAgain = {},
+        endGame = {}
+    )
 }
 
 @Composable
@@ -50,13 +42,25 @@ fun GameScreen(navController: NavController, width: Int, height: Int, bombs: Int
             PlaygroundGenerationUseCase()(bombs, width, height)
         )
     }
-    Playground(
-        playgroundData = playground.value,
-        width = width,
-        height = height,
-        bombs = bombs
-    ) {
+    var gameEnded by rememberSaveable { mutableStateOf(false) }
+    val playAgain: () -> Unit = {
         playground.value = PlaygroundGenerationUseCase()(bombs, width, height)
+        gameEnded = false
+    }
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Playground(
+            playgroundData = playground.value,
+            width = width,
+            height = height,
+            bombs = bombs,
+            endGame = { gameEnded = true },
+            playAgain = playAgain
+        )
+        if (gameEnded) {
+            Button(onClick = playAgain) {
+                Text(text = "Play again")
+            }
+        }
     }
 }
 
@@ -66,6 +70,7 @@ fun Playground(
     width: Int,
     height: Int,
     bombs: Int,
+    endGame: () -> Unit,
     playAgain: () -> Unit
 ) {
     var showLooseMessage by rememberSaveable { mutableStateOf(false) }
@@ -82,13 +87,16 @@ fun Playground(
                         content = playgroundData[rowId][colId].content,
                         opened = playgroundData[rowId][colId].opened.value,
                         markedAsBomb = playgroundData[rowId][colId].markedAsBomb.value,
-                        onClick = {
-                            if (!PlayerClickCheckUseCase(playgroundData)(rowId, colId))
+                        open = {
+                            if (!PlayerClickCheckUseCase(playgroundData)(rowId, colId)) {
                                 showLooseMessage = true
+                                endGame()
+                            }
                             if (playgroundData.flatten()
                                     .count { it.opened.value } == width * height - bombs
                             ) {
                                 showWinMessage = true
+                                endGame()
                             }
                         },
                         markAsBomb = {
@@ -152,39 +160,120 @@ fun Playground(
 }
 
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun Tile(
     content: Char,
     opened: Boolean,
-    onClick: () -> Unit,
+    open: () -> Unit,
     markAsBomb: () -> Unit,
     markedAsBomb: Boolean
 ) {
-    TextButton(
+    MyTextButton(
         modifier = Modifier
-            .size(50.dp)
-            .pointerInput(Unit) {
-                detectTapGestures(
-                    onLongPress = {
-                        markAsBomb()
-                    },
-                    onDoubleTap = {
-                        markAsBomb()
-                    }
-                )
-            },
-        onClick = onClick,
-//        colors = ButtonDefaults.buttonColors(backgroundColor = Color.Cyan)
+            .size(50.dp),
+        onClick = {
+            if (!markedAsBomb) open()
+        },
+        onLongClick = markAsBomb
     ) {
         Text(
             if (markedAsBomb) "?" else if (opened) if (content == '0') " " else content.toString() else "#",
-            fontSize = 20.sp
+            fontSize = 20.sp,
         )
     }
 }
 
-//@Preview
-//@Composable
-//fun TilePreview() {
-//    Tile('1')
-//}
+@Composable
+fun MyTextButton(
+    onClick: () -> Unit,
+    onLongClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+    elevation: ButtonElevation? = null,
+    shape: Shape = MaterialTheme.shapes.small,
+    border: BorderStroke? = null,
+    colors: ButtonColors = ButtonDefaults.textButtonColors(),
+    contentPadding: PaddingValues = ButtonDefaults.TextButtonContentPadding,
+    content: @Composable RowScope.() -> Unit
+) = ButtonWithLongPress(
+    onClick = onClick,
+    modifier = modifier,
+    enabled = enabled,
+    interactionSource = interactionSource,
+    elevation = elevation,
+    shape = shape,
+    border = border,
+    colors = colors,
+    contentPadding = contentPadding,
+    content = content,
+    onLongClick = onLongClick
+)
+
+@OptIn(ExperimentalMaterialApi::class, ExperimentalFoundationApi::class)
+@Composable
+fun ButtonWithLongPress(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit,
+    onDoubleClick: () -> Unit = {},
+    onLongClick: () -> Unit,
+    enabled: Boolean = true,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+    elevation: ButtonElevation? = ButtonDefaults.elevation(),
+    shape: Shape = MaterialTheme.shapes.small,
+    border: BorderStroke? = null,
+    colors: ButtonColors = ButtonDefaults.buttonColors(),
+    contentPadding: PaddingValues = ButtonDefaults.ContentPadding,
+    content: @Composable RowScope.() -> Unit
+) {
+    val contentColor by colors.contentColor(enabled)
+    Surface(
+        onClick = { },
+        modifier = modifier
+            .combinedClickable(
+                interactionSource,
+                rememberRipple(),
+                true,
+                null,
+                Role.Button,
+                null,
+                onClick = { onClick() },
+                onLongClick = { onLongClick() },
+                onDoubleClick = { onDoubleClick() }),
+        enabled = enabled,
+        shape = shape,
+        color = colors.backgroundColor(enabled).value,
+        contentColor = contentColor.copy(alpha = 1f),
+        border = border,
+        elevation = elevation?.elevation(enabled, interactionSource)?.value ?: 0.dp,
+        interactionSource = interactionSource,
+    ) {
+        CompositionLocalProvider(LocalContentAlpha provides contentColor.alpha) {
+            ProvideTextStyle(
+                value = MaterialTheme.typography.button
+            ) {
+                Row(
+                    Modifier
+                        .defaultMinSize(
+                            minWidth = ButtonDefaults.MinWidth,
+                            minHeight = ButtonDefaults.MinHeight
+                        )
+                        .padding(contentPadding)
+                        .combinedClickable(interactionSource,
+                            null,
+                            true,
+                            null,
+                            Role.Button,
+                            null,
+                            onClick = { onClick() },
+                            onLongClick = { onLongClick() },
+                            onDoubleClick = { onDoubleClick() }),
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically,
+                    content = content
+                )
+            }
+        }
+    }
+}
